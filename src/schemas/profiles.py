1262bcfd8b79ff55480d55579e941256b12b2e22
Serhii <serhii.domik@gmail.com>
2025-04-10 19:26:40 +0300

    fixed flake8

--- src/schemas/profiles.py
+++ src/schemas/profiles.py
@@ -19,13 +19,13 @@ class ProfileCreateSchema(BaseModel):
 
     @classmethod
     def from_form(
-            cls,
-            first_name: str = Form(...),
-            last_name: str = Form(...),
-            gender: str = Form(...),
-            date_of_birth: date = Form(...),
-            info: str = Form(...),
-            avatar: UploadFile = File(...)
+        cls,
+        first_name: str = Form(...),
+        last_name: str = Form(...),
+        gender: str = Form(...),
+        date_of_birth: date = Form(...),
+        info: str = Form(...),
+        avatar: UploadFile = File(...),
     ) -> "ProfileCreateSchema":
         return cls(
             first_name=first_name,
@@ -33,7 +33,7 @@ class ProfileCreateSchema(BaseModel):
             gender=gender,
             date_of_birth=date_of_birth,
             info=info,
-            avatar=avatar
+            avatar=avatar,
         )
 
     @field_validator("first_name", "last_name")
@@ -45,14 +45,15 @@ class ProfileCreateSchema(BaseModel):
         except ValueError as e:
             raise HTTPException(
                 status_code=422,
-                detail=[{
-                    "type": "value_error",
-                    "loc": ["first_name" if "first_name" in name else "last_name"],
-                    "msg": str(e),
-                    "input": name
-                }]
+                detail=[
+                    {
+                        "type": "value_error",
+                        "loc": ["first_name" if "first_name" in name else "last_name"],
+                        "msg": str(e),
+                        "input": name,
+                    }
+                ],
             )
-
 
     @field_validator("info")
     @classmethod
@@ -61,15 +62,16 @@ class ProfileCreateSchema(BaseModel):
         if not cleaned_info:
             raise HTTPException(
                 status_code=422,
-                detail=[{
-                    "type": "value_error",
-                    "loc": ["info"],
-                    "msg": "Info field cannot be empty or contain only spaces.",
-                    "input": info
-                }]
+                detail=[
+                    {
+                        "type": "value_error",
+                        "loc": ["info"],
+                        "msg": "Info field cannot be empty or contain only spaces.",
+                        "input": info,
+                    }
+                ],
             )
         return cleaned_info
-
 
     @field_validator("avatar")
     @classmethod
@@ -80,12 +82,14 @@ class ProfileCreateSchema(BaseModel):
         except ValueError as e:
             raise HTTPException(
                 status_code=422,
-                detail=[{
-                    "type": "value_error",
-                    "loc": ["avatar"],
-                    "msg": str(e),
-                    "input": avatar.filename
-                }]
+                detail=[
+                    {
+                        "type": "value_error",
+                        "loc": ["avatar"],
+                        "msg": str(e),
+                        "input": avatar.filename,
+                    }
+                ],
             )
 
     @field_validator("gender")
@@ -97,12 +101,14 @@ class ProfileCreateSchema(BaseModel):
         except ValueError as e:
             raise HTTPException(
                 status_code=422,
-                detail=[{
-                    "type": "value_error",
-                    "loc": ["gender"],
-                    "msg": str(e),
-                    "input": gender
-                }]
+                detail=[
+                    {
+                        "type": "value_error",
+                        "loc": ["gender"],
+                        "msg": str(e),
+                        "input": gender,
+                    }
+                ],
             )
 
     @field_validator("date_of_birth")
@@ -114,12 +120,14 @@ class ProfileCreateSchema(BaseModel):
         except ValueError as e:
             raise HTTPException(
                 status_code=422,
-                detail=[{
-                    "type": "value_error",
-                    "loc": ["date_of_birth"],
-                    "msg": str(e),
-                    "input": str(date_of_birth)
-                }]
+                detail=[
+                    {
+                        "type": "value_error",
+                        "loc": ["date_of_birth"],
+                        "msg": str(e),
+                        "input": str(date_of_birth),
+                    }
+                ],
             )
 
 
